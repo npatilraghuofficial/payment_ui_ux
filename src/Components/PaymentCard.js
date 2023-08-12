4edecb1a1aa994dcb6e486eba560e47b47bbe7ba
npatilraghu <npatilraghu.com>
2023-08-12 21:00:48 +0530

    lazy loading implementes

--- src/Components/PaymentCard.js
+++ src/Components/PaymentCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import Card from 'react-credit-cards';
 import 'react-credit-cards/es/styles-compiled.css';
@@ -6,14 +6,12 @@ import { useNavigate } from 'react-router-dom';
 
 import './PaymentCard.css'; // Import the CSS file you created
 
-const CardWithMaskedNumber = ({ id,number, name, expiry, cvc, zIndex, link }) => {
+const CardWithMaskedNumber = ({ id, number, name, expiry, cvc, zIndex, link }) => {
   const navigate = useNavigate();
 
   const handleCardClick = () => {
-    
-    const selectedCardId = sessionStorage.setItem('selectedCardId', id);
-    // alert(selectedCardId);git 
-    navigate(link); // Navigate to the specified link when the card is clicked
+    sessionStorage.setItem('selectedCardId', id);
+    navigate(link);
   };
 
   return (
@@ -36,41 +34,84 @@ const CardWithMaskedNumber = ({ id,number, name, expiry, cvc, zIndex, link }) =>
   );
 };
 
-export default function App() {
+const PaymentCard = () => {
   const [debitCards, setDebitCards] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [visibleCards, setVisibleCards] = useState(10);
+  const scrollRef = useRef(null);
+  const navigate = useNavigate();
+
   useEffect(() => {
     axios.get('http://localhost:9000/debitCards')
       .then(response => {
         setDebitCards(response.data.reverse());
-        console.log(response.data);
+        setLoading(false);
       })
       .catch(error => {
         console.error('Error fetching data:', error);
+        setLoading(false);
       });
   }, []);
 
+  const handleScroll = () => {
+    if (
+      scrollRef.current &&
+      window.innerHeight + window.scrollY >= scrollRef.current.offsetTop + scrollRef.current.clientHeight
+    ) {
+      setVisibleCards(prevVisibleCards => prevVisibleCards + 10);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  function maskCardNumber(value) {
+    const stringValue = value.toString();
+    let maskedValue = '';
+
+    for (let i = 0; i < stringValue.length; i++) {
+      if (i >= 2 && i < stringValue.length - 4) {
+        maskedValue += '*';
+      } else {
+        maskedValue += stringValue[i];
+      }
+    }
+
+    return maskedValue;
+  }
+
   return (
     <div className="App">
-      <h1>React Credit Cards</h1>
-      <div className="card-list">
-        {debitCards.map((card, index) => (
-          <div
-            key={index}
-            className="card-item"
-            style={{ zIndex: index + 1 }} // Increase the zIndex value
-          >
-            <CardWithMaskedNumber
-              number={card.cardNumber}
-              name={card.cardholderName}
-              expiry={card.expiryDate}
-              cvc={card.cvv}
-              zIndex={index + 1}
-              link={`/CardDetails/${card.id}`}
-            //   onClick ={sessionStorage.setItem('selectedCardId', card.id)}
-            />
-          </div>
-        ))}
+      <h1>React Credit Cards Analysis (LazyLoading)</h1>
+      <div className="card-list" ref={scrollRef}>
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          debitCards.slice(0, visibleCards).map((card, index) => (
+            <div
+              key={index}
+              className="card-item"
+              style={{ zIndex: index + 1 }} // Increase the zIndex value
+            >
+              <CardWithMaskedNumber
+                id={card.id}
+                number={maskCardNumber(card.cardNumber)}
+                name={card.cardholderName}
+                expiry={card.expiryDate}
+                cvc={card.cvv}
+                zIndex={index + 1}
+                link={`/CardDetails/${card.id}`}
+              />
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
-}
+};
+
+export default PaymentCard;
